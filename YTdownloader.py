835dd566c6a4b55d0283f5b2c1a56bc00801c41e
PinelliLuca <p.lucapinelli@gmail.com>
2023-03-03 19:53:20 +0100

    Riscritto da capo con parte grafica

--- YTdownloader.py
+++ YTdownloader.py
@@ -1,18 +1,54 @@
+import time
+import tkinter as tk
 from pytube import YouTube
-from sys import argv
+from tkinter import messagebox
 
-link = argv[1]
-yt = YouTube(link)
+root = tk.Tk()
+root.title("YouTube Downloader")
 
-print("Title: ", yt.title)
-print("Do you want Video or music of this file?")
-print("If you want video press v, if you want audio press a")
 
-if input() == "v":
-    print("VIDEO")
-    ydvideo = yt.streams.get_highest_resolution()
-    ydvideo.download('C:/Users/lpine/PycharmProjects/YouTubeDownloader/Video downloaded')
-elif input() == "a":
-    print("AUDIO")
-    ytaudio = yt.streams.get_audio_only()
-    ytaudio.download('C:/Users/lpine/PycharmProjects/YouTubeDownloader/Audio downloaded')
+# Funzione per il download del video
+def download():
+    try:
+        # Ottieni il link dallo spazio di input di testo
+        link = YouTube(link_entry.get())
+
+        # Ottieni la cartella di destinazione dallo spazio di input di testo
+        destination_folder = destination_entry.get()
+
+        # Scarica il video nella cartella di destinazione
+        video = link.streams.first()
+        video.download(destination_folder)
+
+        # Mostra il messaggio di conferma del download
+        messagebox.showinfo("Download completato", "Il video è stato scaricato con successo!")
+
+    except:
+        # Mostra un messaggio di errore se il download fallisce
+        messagebox.showerror("Errore", "Si è verificato un errore durante il download del video.")
+
+# Dimensioni generiche della finestra
+root.geometry("800x600+100+100")
+
+# Spazio di input di testo per il link del video
+link_label = tk.Label(root,height=2,text="Inserisci il link del video:", font=("Helvetica"))
+link_label.pack()
+link_entry = tk.Entry(root, width=50)
+link_entry.pack()
+
+# Spazio di input di testo per la cartella di destinazione
+destination_label = tk.Label(root,height=2, text="Inserisci la cartella di destinazione:", font=("Helvetica"))
+destination_label.pack()
+destination_entry = tk.Entry(root, width=50)
+destination_entry.pack()
+
+# Aggiungi uno spaziatore verticale
+spacer = tk.Label(root, height=1)
+spacer.pack()
+
+# Bottone per avviare il download
+download_button = tk.Button(root,height=3, text="Download", font=("Helvetica"), command=download)
+download_button.pack()
+
+
+root.mainloop()
